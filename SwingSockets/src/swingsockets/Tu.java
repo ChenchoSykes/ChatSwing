c3bc5ea7ad14ca7f3d296d988795edbd628662d9
Morales Rodriguez Diego Emilio 5IM8 <halo-chencho@live.com.mx>
2016-10-30 21:30:19 -0600

    Add files via upload

---
+++ SwingSockets/src/swingsockets/Tu.java
@@ -0,0 +1,13 @@
+package swingsockets;
+
+/**
+ *
+ * @author halo-_000
+ */
+
+public class Tu {
+ public static void main(String[] args) {
+		Cliento tu = new Cliento();
+		tu.setVisible(true);
+	}   
+}
---
+++ SwingSockets/src/swingsockets/Yo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swingsockets;
+/**
+ *
+ * @author halo-_000
+ */
+public class Yo{
+
+    public static void main(String[] args) {
+
+        Servidor yo = new Servidor();
+        yo.setVisible(true);
+    }
+    
+}
